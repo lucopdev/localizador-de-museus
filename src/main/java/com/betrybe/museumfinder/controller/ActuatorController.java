09f63a2dfe2f9295ed86c00575662b2989066d57
lucopdev <lucopdev@gmail.com>
2023-11-09 17:42:58 -0300

    req 2 e 3

---
+++ src/main/java/com/betrybe/museumfinder/controller/ActuatorController.java
@@ -0,0 +1,10 @@
+package com.betrybe.museumfinder.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actuator/health")
+public class ActuatorController {
+
+}
---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,51 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * The type Museum service.
+ */
+@Service
+public class MuseumService implements MuseumServiceInterface {
+
+  /**
+   * The Database.
+   */
+  MuseumFakeDatabase database;
+
+  /**
+   * Instantiates a new Museum service.
+   *
+   * @param database the database
+   */
+  @Autowired
+  public MuseumService(MuseumFakeDatabase database) {
+    this.database = database;
+  }
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    return null;
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) throws InvalidCoordinateException {
+    Boolean isCoordinateValid = CoordinateUtil.isCoordinateValid(museum.getCoordinate());
+    if (!isCoordinateValid) {
+      throw new InvalidCoordinateException();
+    }
+
+    return database.saveMuseum(museum);
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    return null;
+  }
+}
