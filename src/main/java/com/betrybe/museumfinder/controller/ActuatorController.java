b1f17969131548ecdb85f13453973624190bbfb9
lucopdev <lucopdev@gmail.com>
2023-11-09 17:45:50 -0300

    req 2 e 3

--- src/main/java/com/betrybe/museumfinder/controller/ActuatorController.java
+++ src/main/java/com/betrybe/museumfinder/controller/ActuatorController.java
@@ -3,6 +3,9 @@ package com.betrybe.museumfinder.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The type Actuator controller.
+ */
 @RestController
 @RequestMapping("/actuator/health")
 public class ActuatorController {
